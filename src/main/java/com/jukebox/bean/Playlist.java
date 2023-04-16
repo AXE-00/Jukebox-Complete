e37e5ede113080a4f18ad9ef5772024280a28a47
Ashutosh <ashutosh.k.work@gmail.com>
2023-04-16 18:20:12 +0530

    created Playlist class in bean package - initialized variables and generated getters,setters and toString() method

---
+++ src/main/java/com/jukebox/bean/Playlist.java
@@ -0,0 +1,35 @@
+package com.jukebox.bean;
+
+public class Playlist {
+    private int playlist_id;
+    private String playlist_name;
+
+    public  Playlist(){
+
+    }
+
+    public Playlist(int playlist_id, String playlist_name) {
+        this.playlist_id = playlist_id;
+        this.playlist_name = playlist_name;
+    }
+
+    public int getPlaylist_id() {
+        return playlist_id;
+    }
+
+    public void setPlaylist_id(int playlist_id) {
+        this.playlist_id = playlist_id;
+    }
+
+    public String getPlaylist_name() {
+        return playlist_name;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return " playlist_name='" + playlist_name ;
+    }
+}
+
