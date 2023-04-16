510ef0fb3a8a74011e66c1f8ba3cac82673b5423
Ashutosh <ashutosh.k.work@gmail.com>
2023-04-16 18:23:47 +0530

    created DatabaseConnectionUtil class and connected Mysql to current project

---
+++ src/main/java/com/jukebox/util/DatabaseConnectionUtil.java
@@ -0,0 +1,25 @@
+package com.jukebox.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnectionUtil {
+    public static Connection getConnection(){
+
+        //Load the Drivers
+
+        // Get the connection from database
+        Connection connect = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/jukebox1";
+            String user = "root";
+            String password = "ashutosh0852";
+            connect = DriverManager.getConnection(url,user,password);}
+        catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+        return connect;
+    }
+}
